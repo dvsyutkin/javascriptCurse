d978dfa090656ba11abbfb26a473151c9f453746
Denis <s.d-v@mail.ru>
2019-05-25 16:35:02 +0300

    Второй урок с циклами

--- les1/main.js
+++ les1/main.js
@@ -1,34 +1,42 @@
-var budget;
-var companyName;
-var num = 33721;
+let money = prompt('Ваш бюджет?');
+let companyName = prompt('Название вашего магазина?');
+let time = 23;
 
-var mainList = {
-  budget: 100,
-  companyName: 'Рога и копыта',
+let mainList = {
+  budget: money,
+  companyName: companyName,
   shopGoods: [],
   employers: {},
-  open: true
+  open: false
 };
 
-// mainList.shopGoods[0] = prompt('Какой тип товаров будем продавать?');
-// mainList.shopGoods[1] = prompt('Какой тип товаров будем продавать?');
-// mainList.shopGoods[2] = prompt('Какой тип товаров будем продавать?');
-
-budget = mainList.budget / 30;
-// alert( 'Бюджет на один день = ' + budget );
-
-function mul(number) {
-  let num = number + '';
-  let arr = num.split('');  
-  let result = 1;
-  for (let i = 0; i < arr.length; i++) {
-    result = +arr[i] * result;
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Какой тип товаров будем продавать?');
+  if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50) {
+    console.log('Все верно');
+    mainList.shopGoods[i] = a; 
+  } else {
+    let fault = true;
+    while (fault) {
+      a = prompt('Повторите ввод товара!!!');
+      if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50) {
+        mainList.shopGoods[i] = a;
+        fault = false;
+      }
+    }
   }
-  
-  console.log(Math.pow(result,3));
-  return Math.pow(result,3);
 }
 
+if (time < 0) {
+  console.log('Такого не может быть');
+} else if (time > 8 && time < 20){
+      console.log('Время работать');
+      } else if (time < 24) {
+          console.log('Уже слишком поздно');
+        } else {
+            console.log('В сутках 24 часа');
+          }
 
 
-const p = document.querySelector('.qwe').innerHTML = mul(num);
+alert(mainList.budget / 30);
+console.log(mainList);
